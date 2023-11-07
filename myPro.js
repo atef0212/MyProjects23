42bb1a12e63978862307c1735ee287ab5332b416
atef0212 <che.6178a@gmail.com>
2023-11-07 21:52:17 +0100

    update

---
+++ myPro.js
@@ -0,0 +1,100 @@
+const evolution = [
+  {
+    computerName: "The Analytical Engine",
+    producedDate: 1837,
+    inventedBy: "Charles Babbage",
+    programmed: "Punched cards and manual setup.",
+    capabilities: "General-purpose calculations and programmability.",
+  },
+  {
+    computerName: "Z3",
+    producedDate: 1941,
+    inventedBy: "Konrad Zuse",
+    programmed: "Plugboards and switches.",
+    capabilities:
+      "Specific tasks like cryptography and numerical calculations.",
+  },
+  {
+    computerName: "Colossus",
+    producedDate: 1940,
+    inventedBy: "Developed in the UK during World War II",
+    programmed: "Plugboards and switches.",
+    capabilities: "Designed to break encrypted German messages.",
+  },
+  {
+    computerName: "ENIAC",
+    producedDate: 1945,
+    inventedBy: "John W. Mauchly and J. Presper Eckert",
+    programmed: "Combination of cables and switches.",
+    capabilities:
+      "General-purpose electronic computing for scientific and military applications.",
+  },
+  {
+    computerName: "EDSAC",
+    producedDate: 1949,
+    inventedBy: "Maurice Wilkes",
+    programmed: "Assembly language and simple languages.",
+    capabilities: "Various mathematical and scientific computations.",
+  },
+  {
+    computerName: "UNIVAC I",
+    producedDate: 1951,
+    inventedBy: "Eckert and Mauchly",
+    programmed: "Assembly language and simple languages.",
+    capabilities: "Various mathematical and scientific computations.",
+  },
+  {
+    computerName: "IBM 701",
+    producedDate: 1952,
+    inventedBy: "IBM",
+    programmed: "Assembly languages and higher-level languages like FORTRAN.",
+    capabilities:
+      "Handled complex calculations, data processing, and business applications.",
+  },
+  {
+    computerName: "PDP-8",
+    producedDate: 1965,
+    inventedBy: "Digital Equipment Corporation (DEC)",
+    programmed: "Various programming languages.",
+    capabilities: "Used for scientific, industrial, and business purposes.",
+  },
+  {
+    computerName: "PDP-11",
+    producedDate: 1970,
+    inventedBy: "Digital Equipment Corporation (DEC)",
+    programmed: "Various programming languages.",
+    capabilities: "Used for scientific, industrial, and business purposes.",
+  },
+  {
+    computerName: "Altair 8800",
+    producedDate: 1975,
+    inventedBy: "MITS (Micro Instrumentation and Telemetry Systems)",
+    programmed: "Early operating systems and high-level programming languages.",
+    capabilities:
+      "Brought computing to individual users and facilitated a wide range of applications.",
+  },
+  {
+    computerName: "Sun Microsystems SPARCstation",
+    producedDate: 1989,
+    inventedBy: "Sun Microsystems",
+    programmed:
+      "UNIX-based operating systems and high-performance programming languages.",
+    capabilities: "Developed for scientific and engineering applications.",
+  },
+  {
+    computerName: "Cray-2",
+    producedDate: 1985,
+    inventedBy: "Cray Research",
+    programmed:
+      "UNIX-based operating systems and high-performance programming languages.",
+    capabilities:
+      "Excelled at high-performance simulations and scientific calculations.",
+  },
+  {
+    computerName: "Modern Computers",
+    producedDate: 1990,
+    programmed: "A wide range of programming languages and development tools.",
+    capabilities:
+      "Support a vast array of applications, from web browsing and communication to scientific research and artificial intelligence.",
+  },
+];
